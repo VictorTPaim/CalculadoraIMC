d3944268c8239cca29d6f75cd88ac3d518c3a5e5
Victor Paim <victortpaim@hotmail.com>
2022-04-05 19:13:50 -0300

    Foi incluido os IFs de faixas de imc

--- Program.cs
+++ Program.cs
@@ -20,6 +20,28 @@ namespace CalculadoraIMC
             Console.WriteLine("{0}, sua altura é: {1} e seu peso é: {2}", nome, altura, peso);
 
             Console.WriteLine("Seu IMC é {0}", imc);
+
+            if (imc < 17){
+                Console.WriteLine("Você está muito abaixo do peso");
+            }
+            else if(imc < 18.50){
+                Console.WriteLine("Você está abaixo do peso");
+            }
+            else if(imc < 25){
+                Console.WriteLine("Você está no peso ideal");
+            }
+            else if(imc < 30){
+                Console.WriteLine("Você está acima do peso");
+            }
+            else if(imc < 35){
+                Console.WriteLine("Você está no nivel 1 de obesidade");
+            }
+            else if(imc < 40){
+                Console.WriteLine("Você está no nível 2 de obesidade (severa)");
+            }
+            else{
+                Console.WriteLine("Você está no nível 3 de obesidade (mórbida)");
+            }
         }
     }
 }
