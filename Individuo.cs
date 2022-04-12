d22755f85fc5c63a27f3d42f10a1146e89c6e3f7
Victor Paim <victortpaim@hotmail.com>
2022-04-12 14:46:12 -0300

    Logica implementada na classe Individuo

--- Individuo.cs
+++ Individuo.cs
@@ -3,36 +3,52 @@ public class Individuo
     public String Nome { get; set; }
     public float Altura { get; set; }
     public float Peso { get; set; }
+    public double PesoNecessarioGanhar { get => ((Altura * Altura) * 18.5) - Peso; }
+    public double PesoNecessarioPerder { get => (Peso - (Altura * Altura) * 24.99); }
     public Individuo(String nome, float altura, float peso)
     {
         Nome = nome;
         Altura = altura;
         Peso = peso;
-    }
+    } 
     public double Imc { get => Peso / (Altura * Altura); }
 
-    public string NivelPeso(Double imc){
-        if (imc < 17){
-            Console.WriteLine("Você está muito abaixo do peso");
+    public string NivelPeso(){
+        if (Imc < 17){
+            return "Você está muito abaixo do peso";
         }
-        else if(imc < 18.50){
-            Console.WriteLine("Você está abaixo do peso");
+        else if(Imc < 18.50){
+            return "Você está abaixo do peso";
         }
-        else if(imc < 25){
-            Console.WriteLine("Você está no peso ideal");
+        else if(Imc < 25){
+            return "Você está no peso ideal";
         }
-        else if(imc < 30){
-            Console.WriteLine("Você está acima do peso");
+        else if(Imc < 30){
+            return "Você está acima do peso";
         }
-        else if(imc < 35){
-            Console.WriteLine("Você está no nivel 1 de obesidade");
+        else if(Imc < 35){
+            return "Você está no nivel 1 de obesidade";
         }
-        else if(imc < 40){
-            Console.WriteLine("Você está no nível 2 de obesidade (severa)");
+        else if(Imc < 40){
+            return "Você está no nível 2 de obesidade (severa)";
         }
         else{
-            Console.WriteLine("Você está no nível 3 de obesidade (mórbida)");
+            return "Você está no nível 3 de obesidade (mórbida)";
         }
     }
 
+    public string PesoIdeal()
+    {
+        if(Imc < 18.5)
+        {   
+            return "Você precisa engordar "+ PesoNecessarioGanhar +" quilos para chegar ao IMC ideal.";
+        }
+        if(Imc > 24.99)
+        {           
+            return "Você precisa emagrecer " + PesoNecessarioPerder +" quilos para chegar ao IMC ideal.";
+        }
+        else {
+            return "Parabéns";
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -20,38 +20,10 @@ namespace CalculadoraIMC
 
             Console.WriteLine("{0}, sua altura é: {1} e seu peso é: {2}", nome, altura, peso);
 
-            Console.WriteLine("Seu IMC é {0}", imc);
+            Console.WriteLine(i1.NivelPeso());
 
-            if (imc < 17){
-                Console.WriteLine("Você está muito abaixo do peso");
-            }
-            else if(imc < 18.50){
-                Console.WriteLine("Você está abaixo do peso");
-            }
-            else if(imc < 25){
-                Console.WriteLine("Você está no peso ideal");
-            }
-            else if(imc < 30){
-                Console.WriteLine("Você está acima do peso");
-            }
-            else if(imc < 35){
-                Console.WriteLine("Você está no nivel 1 de obesidade");
-            }
-            else if(imc < 40){
-                Console.WriteLine("Você está no nível 2 de obesidade (severa)");
-            }
-            else{
-                Console.WriteLine("Você está no nível 3 de obesidade (mórbida)");
-            }
-
-            if(imc < 18.5){
-                double pesoNecessario = ((altura * altura) * 18.5) - peso;
-                Console.WriteLine("Você precisa engordar {0} quilos para chegar ao IMC ideal.", pesoNecessario);
-            }
-            if(imc > 24.99){
-                double pesoNecessario = peso - ((altura * altura) * 24.99);
-                Console.WriteLine("Você precisa emagrecer {0} quilos para chegar ao IMC ideal.", pesoNecessario);
-            }
+            Console.WriteLine(i1.PesoIdeal());
+            
         }
     }
 }
